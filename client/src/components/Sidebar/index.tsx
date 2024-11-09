312ec000caac77b6982cec8f7f4bad408f66f5c7
mukhut <mukhutjp@gmail.com>
2024-11-10 00:55:02 +0400

    modi

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -75,7 +75,7 @@ const Sidebar = () => {
         {/* TEAM */}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
           <Image
-            src="https://pm-s3-images.s3.us-east-2.amazonaws.com/logo.png"
+            src="https://pm-s4-images.s3.us-east-1.amazonaws.com/logo.png"
             alt="Logo"
             width={40}
             height={40}
