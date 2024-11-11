c30f28425671134a23af53ebb039b2f570066cf4
mukhut <mukhutjp@gmail.com>
2024-11-11 12:34:09 +0400

    modi

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect } from "react";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
-//import AuthProvider from "./authProvider";
+import AuthProvider from "./authProvider";
 import StoreProvider, { useAppSelector } from "./redux";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
@@ -37,8 +37,11 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 
 export const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
+    
     <StoreProvider>
+       <AuthProvider>
        <DashboardLayout>{children}</DashboardLayout>
+       </AuthProvider>
     </StoreProvider>
   );
 };
